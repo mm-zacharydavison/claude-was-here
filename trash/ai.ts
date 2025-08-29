b869ba1dd421b02a2e6c6f77f69198b0289f875f
Zachary Davison <zachary.davison@meetsmore.com>
2025-08-29 13:33:58 +0200

    Add trash dir for testing human/ai logs and PR squashing.

---
+++ trash/ai.ts
@@ -0,0 +1,3 @@
+console.log('this line was added by AI in commit 1')
+console.log('this line was added by AI in commit 1')
+console.log('this line was added by AI in commit 1')
---
+++ trash/human.ts
@@ -0,0 +1,4 @@
+console.log('this line was added by a human in commit 1')
+console.log('this line was added by AI in commit 1')
+
+console.log('this line was added by a human in commit 1')
